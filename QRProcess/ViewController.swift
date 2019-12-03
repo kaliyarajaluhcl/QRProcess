95ffabcf46d7e1e6d576a65e6e96bfb9d6f44a23
kaliyarajaluhcl <kaliyarajalu.g@hcl.com>
2019-12-03 12:54:55 +0530

    Initial Commit

---
+++ QRProcess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRProcess
+//
+//  Created by Kaliyarajalu G on 03/12/19.
+//  Copyright © 2019 Kaliyarajalu G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
