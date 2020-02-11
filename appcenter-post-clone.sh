2b08506b9de2ebd2896a36ccb0d377dd08faa5a7
kaliyarajaluhcl <kaliyarajalu.g@hcl.com>
2020-02-11 15:58:02 +0530

    Commit with new command shell

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,9 @@
 
 #SetiOS build version
-#/usr/bin/find. -name *Info.plist -exec /usr/libexec/PlistBuddy -c Set :CFBundleVersion {};
-/usr/libexec/PlistBuddy -c 'print ":CFBundleVersion"' Info.plist
+#/usr/bin/find. -name *QRProcess/Info.plist -exec /usr/libexec/PlistBuddy -c Set :CFBundleVersion 1.2 {};
+
+#/usr/libexec/PlistBuddy -c 'print ":$(CURRENT_PROJECT_VERSION)"' Info.plist
+
+#buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_FILE}")
+
+/usr/libexec/PlistBuddy -c 'print ":CFBundleVersion"' QRProcess/Info.plist
